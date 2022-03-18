99aa775f412a69ce4bdcb0ea4907814ba4dd2c5e
AndrusovN <andrusov.n@gmail.com>
2022-03-18 09:35:09 +0300

    change generation algorithm

--- Game.py
+++ Game.py
@@ -1,25 +1,26 @@
 import time
 import random
 
-EASY_PRIMES = [2, 3, 5, 7]
-MEDIUM_PRIMES = [11, 13, 17, 19, 23, 29, 31]
+EASY_PRIMES = [2, 3, 5, 7, 11, 13]
+MEDIUM_PRIMES = [17, 19, 23, 29, 31, 37]
 HARD_PRIMES = [37, 41, 43, 47, 59, 61]
 
-ALL_PRIMES = EASY_PRIMES + MEDIUM_PRIMES + HARD_PRIMES
+ALL_PRIMES = [HARD_PRIMES, MEDIUM_PRIMES, EASY_PRIMES]
 
 
-def generate_dividers(hard_primes_count=1, medium_primes_count=1, easy_primes_count=3):
-    dividers = []
+def generate_dividers(hard_primes_count=1, medium_primes_count=2, easy_primes_count=4):
+    dividers = [[], [], []]
     number = 1
     for i in range(hard_primes_count):
-        dividers.append(random.choice(HARD_PRIMES))
+        dividers[0].append(random.choice(HARD_PRIMES))
     for i in range(medium_primes_count):
-        dividers.append(random.choice(MEDIUM_PRIMES))
+        dividers[1].append(random.choice(MEDIUM_PRIMES))
     for i in range(easy_primes_count):
-        dividers.append(random.choice(EASY_PRIMES))
+        dividers[2].append(random.choice(EASY_PRIMES))
 
     for d in dividers:
-        number *= d
+        for div in d:
+            number *= div
 
     return dividers, number
 
@@ -30,40 +31,39 @@ class Game:
         self.mistake_callback = mistake_callback
         self.finish_callback = finish_callback
 
-        self.client_progress = [0, 0]
+        self.client_progress = [[2, 0], [2, 0]]
         self.client_finish_time = [-1, -1]
         self.client_mistakes = [0, 0]
         self.begin_time = time.time()
         self.dividers, self.number = generate_dividers()
         self.client_numbers = [self.number, self.number]
-        random.shuffle(self.dividers)
+        for i in range(3):
+            random.shuffle(self.dividers[i])
 
     def get_question(self, player_id):
         dividers_begin_index = self.client_progress[player_id]
 
-        if dividers_begin_index == len(self.dividers):
+        if dividers_begin_index[0] == len(self.dividers):
             return None, 1
 
-        variants = []
-        var1 = random.choice(ALL_PRIMES)
-        while var1 in self.dividers[dividers_begin_index:]:
-            var1 = random.choice(ALL_PRIMES)
-        variants.append(var1)
-        var2 = random.choice(ALL_PRIMES)
-        while var2 in self.dividers[dividers_begin_index:] + [var1]:
-            var2 = random.choice(ALL_PRIMES)
-        variants.append(var2)
-        variants.append(self.dividers[dividers_begin_index])
-        random.shuffle(variants)
+        variants = [self.dividers[self.client_progress[player_id][0]][self.client_progress[player_id][1]]]
+        while len(variants) < 3:
+            variant = random.choice(ALL_PRIMES[self.client_progress[player_id][0]])
+            if self.client_numbers[player_id] % variant != 0:
+                variants.append(variant)
         return variants, self.client_numbers[player_id]
 
     def answer_question(self, player_id, divider, points_me, points_enemy):
-        if divider == self.dividers[self.client_progress[player_id]]:
-            self.client_numbers[player_id] //= self.dividers[self.client_progress[player_id]]
-            self.client_progress[player_id] += 1
+        if divider == self.dividers[self.client_progress[player_id][0]][self.client_progress[player_id][1]]:
+            self.client_numbers[player_id] //= self.dividers[self.client_progress[player_id][0]]\
+                [self.client_progress[player_id][1]]
+            self.client_progress[player_id][1] += 1
+            if self.client_progress[player_id][1] == len(self.dividers[self.client_progress[player_id][0]]):
+                self.client_progress[player_id][0] -= 1
+                self.client_progress[player_id][1] = 0
             self.answer_callback(player_id)
 
-            if self.client_progress[player_id] == len(self.dividers):
+            if self.client_numbers[player_id] == 1:
                 print('Yeah it\'s finish')
                 self.client_finish_time[player_id] = time.time()
                 if points_me > points_enemy:
